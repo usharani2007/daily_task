b7cda4ac3b7b6e221d956b4b4774d481cb6dc6ad
usharani2007 <mejariusharani6@gmail.com>
2026-02-12 19:53:26 +0530

    integrr()

---
+++ integerr.py
@@ -0,0 +1,41 @@
+num=949321
+print(num)
+print(type(num))
+
+binary=bin(num)
+print("binary format is : " , binary)
+
+octal=oct(num)
+print("octal format is : " , octal)
+print(type(octal))
+
+
+hex_decimal=hex(num)
+print(type(hex_decimal))
+print("hex format is : ", hex_decimal)
+
+
+complex_number=10+9j
+print("complex number is : " , complex_number)
+print(type(complex_number))
+
+
+cgpa1=8.9
+cgpa2=9.9
+print(cgpa1,cgpa2)
+print(type(cgpa1))
+print("addtion of floating ",cgpa1+cgpa2)
+print("sub of floating",cgpa1-cgpa2)
+print("multplication of floating",cgpa1*cgpa2)
+print("division of floating",cgpa1/cgpa2)
+
+complex_number1=99+85j
+complex_number2=complex(67,5)
+print(complex_number1,complex_number2)
+print("addition of complex_number",complex_number1+complex_number1)
+print("sub of complex_number",complex_number1-complex_number2)
+print("multiplication of complex_number",complex_number1*complex_number2)
+print("division of complex_number",complex_number1/complex_number2)
+
+
+
