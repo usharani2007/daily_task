caeb4dde6850090dde30b380c673f3df2b45ddb5
usharani2007 <mejariusharani6@gmail.com>
2026-03-16 19:52:47 +0530

    elif

---
+++ elif.py
@@ -0,0 +1,15 @@
+hotel_rating=float(input("entre the rating:"))
+if hotel_rating==4.0:
+           print("cost is more")
+           print("food is good")
+           print("ambinations is good")
+elif hotel_rating==4.3:
+         print("cost is meidum")
+         print("food is avg")
+         print("cleaning is good")
+elif hotel_rating==4.0:
+        print("cost is medium")
+        print("food is bad")
+        print("cleaning is avg")
+else:
+       print("street food")
---
+++ if.py
@@ -0,0 +1,12 @@
+employe_1="usha"
+employe_2="shyam"
+interview={employe_1:True,employe_2:flase}
+if interview[employe_1]:
+     print("selected it selcetor")
+else:
+     print("note selected")    
+
+if interview[employe_2]:
+    print("selected it selcetor")
+else:
+    print("note selected")    
